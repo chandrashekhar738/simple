ded7d7cd514af51dbe3d09c6347ca53d0b53a48f
chandru <chandrashekharngowda2002@gmail.com>
2026-03-26 10:06:26 +0530

    Fix test_sub function definition

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ def test_add():
   assert add(2,3)==5
   assert add(-1,1)==0
   assert add(0,0)==0
-def test_sub(a,b):
+def test_sub():
   assert sub(5,3)==2
   assert sub(0,5)==-5
   assert sub(-1,-1)==0
