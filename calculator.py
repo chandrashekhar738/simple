ec62dcefc5d4c3eff1689dd395f8532b4b3b6f44
chandru <chandrashekharngowda2002@gmail.com>
2026-03-26 09:57:28 +0530

    Add docstrings to add and sub functions
    Added docstrings to the add and sub functions.

--- calculator.py
+++ calculator.py
@@ -4,6 +4,8 @@ This module contains basic calculator functions.
 
 
 def add(a,b):
+    """Return the sum of two numbers."""
     return a+b
 def sub(a,b):
+    """Return the subtraction of two numbers."""
     return a-b
